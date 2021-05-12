319914c8740e748af19ca9b673dc400062c482b2
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-05-12 09:37:07 -0500

    Initial setup: create particles and update them

--- master.js
+++ master.js
@@ -1,4 +1,10 @@
-let WIDTH, HEIGHT;
+let particles = [],
+    WIDTH,
+    HEIGHT;
+
+const n = 150,
+    minVel = 2,
+    maxVel = 4;
 
 const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
@@ -16,6 +22,12 @@ function init() {
     canvas.setAttribute("width", WIDTH);
     canvas.setAttribute("height", HEIGHT);
 
+    for (let i = 0; i < n; i++) {
+        const pos = new Vector(random(WIDTH), random(HEIGHT));
+        const vel = Vector.random().setMag(random(minVel, maxVel));
+        particles.push(new Particle(pos, vel));
+    }
+
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.beginPath();
     ctx.fillStyle = "black";
@@ -26,7 +38,15 @@ function init() {
 }
 
 function ani() {
-    //requestAnimationFrame(ani);
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    for (const p of particles) {
+        p.update();
+        p.borders(1);
+        p.show();
+    }
+
+    requestAnimationFrame(ani);
 }
 
 init();
